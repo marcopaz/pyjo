f6ac9ae5b735c4fd35f358dd30cf0ead5eff270c
Marco Pazzaglia <marco@pazzaglia.me>
2019-08-29 15:46:04 +0200

    Allow to use enum value instead of enum name

--- pyjo/fields/enumfield.py
+++ pyjo/fields/enumfield.py
@@ -2,18 +2,19 @@ from pyjo.fields.field import Field
 
 
 class EnumField(Field):
-    def __init__(self, enum, **kwargs):
+    def __init__(self, enum, use_name=True, **kwargs):
         """
         :type enum: T
         :rtype: T
         """
         super(EnumField, self).__init__(type=enum, **kwargs)
         self.enum_cls = enum
+        self.use_name = use_name
 
     def to_dict(self, value):
         if value is not None:
-            return value.name
+            return value.name if self.use_name else value.value
 
     def from_dict(self, name):
         if name is not None:
-            return self.enum_cls[name]
+            return self.enum_cls[name] if self.use_name else self.enum_cls(name)
